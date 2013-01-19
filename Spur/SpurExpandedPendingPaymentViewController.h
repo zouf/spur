783f4ad0aec1196f1d254c27499c0b22e380bdd6
Matt Zoufaly <mattzouf@gmail.com>
2013-01-19 15:45:50 -0500

    Add Libraries/Venmo to Xcode project.

--- Spur/SpurExpandedPendingPaymentViewController.h
+++ Spur/SpurExpandedPendingPaymentViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SpurExpandedPendingPaymentViewController : UIViewController
+@interface SpurExpandedPendingPaymentViewController : UITableViewController
 
 @end
--- Spur/SpurExpandedPendingPaymentViewController.m
+++ Spur/SpurExpandedPendingPaymentViewController.m
@@ -35,4 +35,34 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(UIView*)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    if(section==0)
+    {
+        UIView *v = [[UIView alloc]initWithFrame:CGRectMake(0,0,320,100)];
+        UILabel * lbl = [[UILabel alloc]initWithFrame:CGRectMake(0,50,50,50)];
+        lbl.text = @"Actions to take for the transaction";
+        [v addSubview:lbl];
+    }
+}
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if(indexPath.row == 0)
+    {
+        NSLog(@"Sel 0");
+    
+    }
+    else if (indexPath.row ==1 )
+    {
+        NSLog(@"Sel 1");
+    }
+    else
+    {
+        NSLog(@"Sel 2");
+
+    }
+}
+
+
+
 @end
